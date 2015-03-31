34304202f56fb1f87a1e12e20bf51e44c1f00c2b
Scott Morey <swmorey@gmail.com>
2015-03-31 21:04:40 +0000

    first stab at acaching

--- app.rb
+++ app.rb
@@ -4,10 +4,22 @@ require 'sinatra/cross_origin'
 require 'open-uri'
 require 'json'
 require 'library_stdnums'
+require 'dalli'
 
-#Settings for development. Comment out on deploy
-set :port, 3000
-set :bind, '0.0.0.0'
+configure :development, :test do
+	set :port, 3000
+	set :bind, '0.0.0.0'
+end
+
+configure :production do
+cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    })
+end
 
 set :allow_origin, :any
 set :allow_methods, [:get, :post, :options]
@@ -17,24 +29,30 @@ route :get, :post, '/' do
 	cross_origin
 	content_type :json
 	if params[:isbn]
-		isbns  = ''
-		params[:isbn].split(',').each do |i|
-			isbn = StdNum::ISBN.normalize(i) rescue nil
-			if isbn
-				isbns = isbns + ',' + isbn
+		message = cache.get(params[:isbn]) rescue nil
+		if !message
+			isbns  = ''
+			params[:isbn].split(',').each do |i|
+				isbn = StdNum::ISBN.normalize(i) rescue nil
+				if isbn
+					isbns = isbns + ',' + isbn
+				end
 			end
-		end
-		request_url = 'https://www.goodreads.com/book/review_counts.json?isbns=' + params[:isbn]
-		reviews =  JSON.parse(open(request_url).read) rescue nil
-		if reviews
-			raw = reviews["books"][0]["average_rating"]
-			rounded = (raw.to_f * 2).round / 2.0
-			stars = rating_to_stars(rounded)
-			gr_id = reviews["books"][0]["id"]
-			gr_link = 'https://www.goodreads.com/book/show/' + gr_id.to_s
-			message = {:gr_id => gr_id, :raw => raw, :rounded => rounded, :stars_html => stars, :gr_link => gr_link}
-		else
-			message = {:message => "no reviews found"}
+			request_url = 'https://www.goodreads.com/book/review_counts.json?isbns=' + params[:isbn]
+			reviews =  JSON.parse(open(request_url).read) rescue nil
+			if reviews
+				raw = reviews["books"][0]["average_rating"]
+				rounded = (raw.to_f * 2).round / 2.0
+				stars = rating_to_stars(rounded)
+				gr_id = reviews["books"][0]["id"]
+				gr_link = 'https://www.goodreads.com/book/show/' + gr_id.to_s
+				message = {:gr_id => gr_id, :raw => raw, :rounded => rounded, :stars_html => stars, :gr_link => gr_link}
+			else
+				message = {:message => "no reviews found"}
+			end
+			if self.class.production?
+				cache.set(params[:isbn], "cached you smarty!")
+			end
 		end
 	else
 	   message = {:message => "error"}
